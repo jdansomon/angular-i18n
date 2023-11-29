c48cd3a234e465c6da8c623660bfc19493b20f0b
j.dansomon <j.dansomon@multivers.dev>
2023-11-29 13:18:03 +0000

    add @angular/localize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule, NgPlural} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgPlural],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-i18n';
+  tasksCount = 3;
+  minutes = 0;
+  gender = 'female';
+  fly = true;
+  logo = 'https://angular.io/assets/images/logos/angular/angular.png';
+
+  inc(i: number) {
+    this.minutes = Math.min(5, Math.max(0, this.minutes + i));
+  }
+
+  male() {
+    this.gender = 'male';
+  }
+
+  female() {
+    this.gender = 'female';
+  }
+
+  other() {
+    this.gender = 'other';
+  }
 }
